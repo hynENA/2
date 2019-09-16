8c7332a73ce03e22cb573345829e13b3cb2dc86a
hynENA <44791123+hynENA@users.noreply.github.com>
2019-09-16 20:57:30 +0800

    Add files via upload

---
+++ SalesTax.java
@@ -0,0 +1,16 @@
+package helloworld1;
+import java.util.Scanner;//Scanner is in the java.util package
+public class SalesTax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter purchase amount: ");
+		double purchaseAmount = input.nextDouble();
+		
+		double tax = purchaseAmount * 0.06;
+		System.out.println("Sales tax is $" + (int)(tax * 100)/100.0);
+	}
+
+}
